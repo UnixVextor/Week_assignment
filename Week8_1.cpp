5546bd1a05be04b85601a43eca78491b68e44edd
unitvector2021 <vitoon.sdf@gmail.com>
2021-09-19 21:36:05 +0700

    add Week 8

---
+++ Week8_1.cpp
@@ -0,0 +1,19 @@
+/*จงเขียนโปรแกรมเพื่อรับข้อมูลเป็น string ไปเรื่อยๆ จนกว่าจะพิมพ์เครื่องหมาย . หลังจากนั้นให้จัดเก็บข้อมูลลงในไฟล์ชื่อ c:\temp\data.txt ดังตัวอย่าง (Level 4)
+*/
+#include <iostream>
+#include<string>
+#include <fstream>
+#include<direct.h>
+using namespace std;
+int main(){
+    mkdir("c:/temp");
+    fstream file;
+    file.open("c:\\temp\\data.txt",ios::out);
+    string text = " ";
+    cout << "Input data string:\n";
+    while(text != "."){
+        cin >> text ;
+        file << text << endl;
+    }
+    file.close();
+}
